a1a4321ea791513f60fc9a92c87d505021f8ccd6
wuyudi <wuyudi1109@gmail.com>
2020-07-22 21:13:27 +0800

    Update 翻译工具.py

--- 翻译工具/翻译工具.py
+++ 翻译工具/翻译工具.py
@@ -1,16 +1,12 @@
-中文文件 = open(r'C:\Users\wuyudi\Desktop\temp1.txt',
-            encoding='utf-8').readlines()
-英文文件 = open(r'C:\Users\wuyudi\Desktop\temp2.txt',
-            encoding='utf-8').readlines()
+中 = r'C:\Users\wuyudi\Desktop\temp1.txt'
+英 = r'C:\Users\wuyudi\Desktop\temp2.txt'
+with open(中, encoding='utf-8') as 中文文件, open(英, encoding='utf-8') as 英文文件:
+    def newfile():
+        for i, j in enumerate(zip(英文文件, 中文文件)):
+            yield j[0]
+            if i % 4 == 2:
+                yield j[1]
 
-
-def newfile():
-    for i, j in enumerate(英文文件):
-        yield j
-        if i % 4 == 2:
-            yield 中文文件[i]
-
-
-with open(r'C:\Users\wuyudi\Desktop\34 - 副本.txt', 'w') as f:
-    for k in newfile():
-        f.write(k)
+    with open(r'C:\Users\wuyudi\Desktop\34 - 副本.txt', 'w') as f:
+        for k in newfile():
+            f.write(k)
