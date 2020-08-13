061ee5b5a45d61eab00a5c94555963ac4e247ced
wuyudi <wuyudi1109@gmail.com>
2020-08-14 00:22:13 +0800

    Update 翻译工具.py

--- 翻译工具/翻译工具.py
+++ 翻译工具/翻译工具.py
@@ -1,16 +1,15 @@
 from functools import partial
-
+from itertools import cycle
 英 = r'C:\Users\wuyudi\Desktop\temp1.txt'
 中 = r'C:\Users\wuyudi\Desktop\temp2.txt'
 输出 = r'C:\Users\wuyudi\Desktop\34 - 副本.txt'
 
 Open = partial(open, encoding='utf-8')
-中文文件, 英文文件, f = Open(中), Open(英), Open(输出, 'w')
-with 中文文件, 英文文件, f:
+with Open(中) as 中文文件, Open(英) as 英文文件, Open(输出, 'w') as f:
     def newfile():
-        for i, (英文, 中文) in enumerate(zip(英文文件, 中文文件)):
+        for i, 英文, 中文 in zip(cycle(range(4)), 英文文件, 中文文件):
             yield 英文
-            if i % 4 == 2:
+            if i == 2:
                 yield 中文
     for k in newfile():
         f.write(k)
